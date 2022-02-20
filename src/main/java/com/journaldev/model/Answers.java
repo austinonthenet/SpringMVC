da2649c401d91a4fec15a0140c9175fed6463ec3
Resmy <resmyrose@gmail.com>
2022-02-20 14:48:05 -0800

    Added two more files

--- src/main/java/com/journaldev/model/Answers.java
+++ src/main/java/com/journaldev/model/Answers.java
@@ -1,5 +1,47 @@
 package com.journaldev.model;
-
+/*
+ * answer_id SERIAL PRIMARY KEY,
+	question_id int,
+	answer_text TEXT,
+	is_correct_answer boolean,
+	marks int*/
 public class Answers {
+	private int answerId;
+	private int questionId;
+	private String answerText;
+	private boolean isCorrectAnswer;
+	private int marks;
+	public int getAnswerId() {
+		return answerId;
+	}
+	public void setAnswerId(int answerId) {
+		this.answerId = answerId;
+	}
+	public int getQuestionId() {
+		return questionId;
+	}
+	public void setQuestionId(int questionId) {
+		this.questionId = questionId;
+	}
+	public String getAnswerText() {
+		return answerText;
+	}
+	public void setAnswerText(String answerText) {
+		this.answerText = answerText;
+	}
+	public boolean isCorrectAnswer() {
+		return isCorrectAnswer;
+	}
+	public void setCorrectAnswer(boolean isCorrectAnswer) {
+		this.isCorrectAnswer = isCorrectAnswer;
+	}
+	public int getMarks() {
+		return marks;
+	}
+	public void setMarks(int marks) {
+		this.marks = marks;
+	}
+	
+	
 
 }
--- src/main/java/com/journaldev/model/Test.java
+++ src/main/java/com/journaldev/model/Test.java
@@ -1,5 +1,76 @@
 package com.journaldev.model;
 
+import java.util.Date;
+
+/*
+ * test_id SERIAL PRIMARY KEY,
+    name VARCHAR(50) not null,
+    deccription VARCHAR(200),
+    category_id int,
+    date_created TIMESTAMP,
+    passing_score int,
+    time_limit_mins int,
+    questions_limit int*/
+
 public class Test {
+	private long testId;
+	private String testName;
+	private String description;
+	private int categoryId;
+	private Date dateCreated;
+	private int passingScore;
+	private int timeLimitMins;
+	private int questionsLimit;
+	public long getTestId() {
+		return testId;
+	}
+	public void setTestId(long testId) {
+		this.testId = testId;
+	}
+	public String getTestName() {
+		return testName;
+	}
+	public void setTestName(String testName) {
+		this.testName = testName;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getCategoryId() {
+		return categoryId;
+	}
+	public void setCategoryId(int categoryId) {
+		this.categoryId = categoryId;
+	}
+	public Date getDateCreated() {
+		return dateCreated;
+	}
+	public void setDateCreated(Date dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+	public int getPassingScore() {
+		return passingScore;
+	}
+	public void setPassingScore(int passingScore) {
+		this.passingScore = passingScore;
+	}
+	public int getTimeLimitMins() {
+		return timeLimitMins;
+	}
+	public void setTimeLimitMins(int timeLimitMins) {
+		this.timeLimitMins = timeLimitMins;
+	}
+	public int getQuestionsLimit() {
+		return questionsLimit;
+	}
+	public void setQuestionsLimit(int questionsLimit) {
+		this.questionsLimit = questionsLimit;
+	}
+	
+	
+	
 
 }
